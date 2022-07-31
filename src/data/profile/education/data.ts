6f88c1f1a207188f252f6674ef2bf547f98ec055
mahiro72 <mahiro.yoshie@gmail.com>
2022-08-01 01:46:19 +0900

    add: doer

--- src/data/profile/education/data.ts
+++ src/data/profile/education/data.ts
@@ -1,5 +1,6 @@
 import img_doshisha from "../../../img/profile/education/doshisha.png";
 import img_42tokyo from "../../../img/profile/education/42tokyo.jpg";
+import img_doer from "../../../img/profile/education/doer.png";
 
 const data = {
     "education":[
@@ -8,6 +9,12 @@ const data = {
             "img":img_42tokyo,
             "name":"42Tokyo",
             "about":"libcやprintfの実装に取り組んでいます"
+        },
+        {
+            "date":"2022/3 ~",
+            "img":img_doer,
+            "name":"Do'er",
+            "about":"同志社大学・同志社女子大学エンジニアのためのコミュニティ / 運営"
         },
         {
             "date":"2019/4",
