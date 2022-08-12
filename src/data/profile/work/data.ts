cd69fb756001ed5f7a3f6cee26556b57288d20cf
mahiro72 <mahiro.yoshie@gmail.com>
2022-08-13 01:20:52 +0900

    add Plaid

--- src/data/profile/work/data.ts
+++ src/data/profile/work/data.ts
@@ -2,26 +2,33 @@ import img_zozo from "../../../img/profile/work/zozo.png"
 import img_example from "../../../img/profile/work/company_example.png"
 import img_teamlab from "../../../img/profile/work/teamlab.png"
 import img_cookpad from "../../../img/profile/work/cookpad.png"
+import img_plaid from "../../../img/profile/work/plaid.png"
 
 const data = {
     "work":[
         {
+            "date":"2022/8 -",
+            "img":img_plaid,
+            "name":"PLAID",
+            "about":"Software Engineer"
+        },
+        {
             "date":"2022/6 -",
             "img":img_zozo,
             "name":"ZOZO",
-            "about":"バックエンドエンジニア"
+            "about":"Backend Engineer"
         },
         {
             "date":"2021/4 - 2022/7",
             "img":img_example,
             "name":"Sou.co",
-            "about":"フルスタックエンジニア. HRtechやFintechの開発"
+            "about":"Fullstack Engineer. HRtechやFintechの開発"
         },
         {
             "date":"2022/3",
             "img":img_teamlab,
             "name":"TeamLab",
-            "about":"フロントエンドエンジニア. システム設計やテスト,実現可能性検討などの経験をつみました"
+            "about":"Frontend Engineer. システム設計やテスト,実現可能性検討などの経験をつみました"
         },
         {
             "date":"2021/9",
