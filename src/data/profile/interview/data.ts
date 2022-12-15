744c79811538851c32afced88b9f2206bcc11de4
mahiro72 <mahiro.yoshie@gmail.com>
2022-12-15 11:55:40 +0900

    fix: 順番入れかえ

--- src/data/profile/interview/data.ts
+++ src/data/profile/interview/data.ts
@@ -2,14 +2,14 @@
 const data = {
     "interview":[
         {
-            "title":"快適なオンライン飲み会体験を提供する「のみぱら」がＪストリーム賞を受賞 ー JPHACKS2022",
-            "date":"2022/11",
-            "link":"https://voice.stream.co.jp/event/20221129/"
-        },
-        {
             "title":"未来のエンジニアを育てる「技育展」にスポンサーとして初参加しました！",
             "date":"2022/12",
             "link":"https://blog.brainpad.co.jp/entry/2022/12/08/150021"
+        },
+        {
+            "title":"快適なオンライン飲み会体験を提供する「のみぱら」がＪストリーム賞を受賞 ー JPHACKS2022",
+            "date":"2022/11",
+            "link":"https://voice.stream.co.jp/event/20221129/"
         },
     ]
 }
